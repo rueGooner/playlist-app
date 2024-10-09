b09c3c10ffab3ad928ade209045450e040b34f81
rue <rue.cumberbatch@bt.com>
2024-10-09 15:00:11 +0100

    feat: theme up the guest layout

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -5,16 +5,16 @@ import { Link } from '@inertiajs/vue3';
 
 <template>
   <div
-    class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900"
+    class="flex min-h-screen flex-col items-center bg-primary pt-6 sm:justify-center sm:pt-0 "
   >
     <div>
       <Link href="/">
-        <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
+        <ApplicationLogo class="h-20 w-20 fill-current text-highlight" />
       </Link>
     </div>
 
     <div
-      class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800"
+      class="mt-6 w-full overflow-hidden bg-primary px-6 py-4 sm:max-w-md sm:rounded-lg ring-1 ring-highlight/50 hover:ring-highlight shadow-xl"
     >
       <slot />
     </div>
