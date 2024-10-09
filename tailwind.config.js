4388c164b44e358bd4358eec1f417d5257be336a
rue <rue.cumberbatch@bt.com>
2024-10-09 14:55:00 +0100

    feat: import themes from themes.ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,26 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import {blueGreens, icyBlues, myTheme} from "./resources/themes/theme.ts";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-    ],
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.vue',
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Advent Pro', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        ...myTheme
+      }
     },
+  },
 
-    plugins: [forms],
+  plugins: [forms],
 };
