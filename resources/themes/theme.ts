08b704b8e5a1e4c084af22e9f4acf4ca23ff0579
rue <rue.cumberbatch@bt.com>
2024-10-09 14:54:46 +0100

    feat: add custom themes and new content

---
+++ resources/themes/theme.ts
@@ -0,0 +1,27 @@
+export const blueGreens = {
+  primary: '#021C1E',
+  secondary: '#004445',
+  accent: '#2C7873',
+  highlight: '#6FB98F'
+}
+
+export const icyBlues = {
+  primary: '#F1F1F2',
+  secondary: '#BCBABE',
+  accent: '#A1D6E2',
+  highlight: '#1995AD'
+}
+
+export const birdsBerries = {
+  primary: '#9A9EAB',
+  secondary: '#5D5E5E',
+  accent: '#EC96A4',
+  highlight: '#DFE166'
+}
+
+export const myTheme = {
+  primary: '#404040',
+  secondary: '#FFAA00',
+  accent: '#00AAFF',
+  highlight: '#E4535E'
+}
