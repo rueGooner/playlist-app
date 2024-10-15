74c6113c5a01f31d86774a847ed422f90787b858
rue <rue.cumberbatch@bt.com>
2024-10-15 01:27:53 +0100

    feat: rollback and migrate

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->enum('role', ['admin', 'client'])->default('client');
+            $table->enum('role', ['admin', 'client', 'dj'])->default('client');
             $table->timestamps();
         });
 
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,22 +8,29 @@ use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        // 1. Admin User
-        User::create([
-            'name' => 'Admin',
-            'email' => 'ruebencee@gmail.com',
-            'password' => Hash::make('i6a8uejKJc!'),
-            'email_verified_at' => now(),
-            'role' => 'admin'
-        ]);
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    // 1. Admin User
+    User::create([
+      'name' => 'Admin',
+      'email' => 'ruebencee@gmail.com',
+      'password' => Hash::make('i6a8uejKJc!'),
+      'email_verified_at' => now(),
+      'role' => 'admin'
+    ]);
 
-        User::factory()->count(9)->create([
-            'role' => 'client',
-        ]);
-    }
+    $clientCount = rand(50, 150);
+    $djCount = 15;
+
+    User::factory()->count($clientCount)->create([
+      'role' => 'client',
+    ]);
+
+    User::factory()->count($djCount)->create([
+      'role' => 'dj',
+    ]);
+  }
 }
