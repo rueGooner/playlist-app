174f497d0cd4f523338cc04afd1662c477dc0c2d
rue <rue.cumberbatch@bt.com>
2024-10-16 23:35:56 +0100

    feat: create all migration schemas

---
+++ database/migrations/2024_10_16_222840_create_songs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('songs', function (Blueprint $table) {
+      $table->id();
+      $table->string('track_name');
+      $table->string('artist');
+      $table->string('album_name');
+      $table->string('genre');
+      $table->string('released_date');
+      $table->timestamps();
+    });
+
+    Schema::create('catalogs', function (Blueprint $table) {
+      $table->id();
+      $table->string('name')->unique();
+      $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Reference to the DJ
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('songs');
+    Schema::dropIfExists('catalogs');
+  }
+};
